0666e8fe85488455be999412503f34005fef413d
Jeremy Kelley <jeremy@33ad.org>
2015-03-11 17:24:16 -0500

    new MemConnection type does in memory data stores and map based queries using pyexecjs

--- cushion/persist.py
+++ cushion/persist.py
@@ -1,8 +1,10 @@
 
+import operator
 from json import dumps, loads
 from textwrap import dedent
 from uuid import uuid4
 
+import execjs
 from couchbase import Couchbase
 
 ActiveConnection = None
@@ -25,18 +27,42 @@ class BaseConnection(object):
     pass
 
 
-
-
-emitjs = '''
-var output_queue = {}
-var emit = function(key, val) {
-    if (output_queue[key]) {
-        output_queue[key].push(val)
-    } else {
-        output_queue[key] = [ val ]
+mapwrap = '''
+function map_wrapper(doc, meta, outq) {
+    function emit(key, val) {
+        emit.outq.push([key, val, meta._id])
     }
+    emit.outq = outq
+    var mapf = %MAPF%
+    mapf(doc, meta)
+    return emit.outq
 }
 '''
+
+
+class MemDoc(object):
+    def __init__(self, docid, value):
+        self.key = docid
+        self.value = value
+
+
+class MemResult(object):
+    def __init__(self, key, docid, value, doc=None):
+        self.key = key
+        self.docid = docid
+        self.value = value
+        self.doc = doc
+
+
+class MemResultSet(object):
+    def __init__(self, results, include_docs=False):
+        self._results = results
+        self.include_docs = include_docs
+
+    def __iter__(self):
+        for r in self._results:
+            yield r
+
 
 class MemConnection(BaseConnection):
 
@@ -48,39 +74,57 @@ class MemConnection(BaseConnection):
         return self.data.get(key, None)
 
     def set(self, key, value):
+        if key is None:
+            key = uuid4().hex
         self.data[key] = value
+        return key, uuid4().hex # fake cas
 
     def delete(self, key):
         del self.data[key]
 
     def query(self, design, name, **kw):
-        # NO REDUCE YET
+        # **note** NO REDUCE YET
         view_key = '/'.join((design, name))
         if view_key not in self.designs:
             raise Exception('view not found')
         view = self.designs[view_key]
-        mapf_ctx = view.get('map')
-        redf_ctx = view.get('reduce')
+        mapf_ctx = view.get('mapf')
+        redf_ctx = view.get('redf')
+        include_docs = False
+        if 'include_docs' in kw:
+            if kw['include_docs'] and kw['include_docs'] is not 'false':
+                include_docs = True
+        descending = bool(kw.get('descending', False))
+        if descending:
+            cmpop = operator.ge
+        else:
+            cmpop = operator.le
+        outq = []
+        # each entry looks like  [key, val, _id]
         for k,d in self.data.iteritems():
             meta = {'_id': k}
-            doc = d
-            mapf_ctx.call('mapf', doc, meta)
-        map_results = mapf.eval("output_queue")
-        if kw.get('include_docs'):
-            ret = {}
-            for k in map_results.keys():
-                ret[k] = self.data[k]
-            return ret
-        else:
-            return map_results
+            outq = mapf_ctx.call('map_wrapper', d, meta, outq)
+        if 'key' in kw:
+            outq = filter(lambda x: cmp(x[0], kw['key'])==0, outq)
+        if 'startkey' in kw:
+            outq = filter(lambda x: cmpop(cmp(kw['startkey'], x[0]),0), outq)
+        if 'endkey' in kw:
+            outq = filter(lambda x: cmpop(cmp(x[0], kw['endkey']),0), outq)
+        outq.sort(key=operator.itemgetter(0), reverse=descending)
+        results = []
+        for k in outq:
+            r_ = MemResult(key=k[0], docid=k[2], value=k[1])
+            if include_docs:
+                r_.doc = MemDoc(k[2], self.data[k[2]])
+            results.append(r_)
+        if 'limit' in kw:
+            results = results[:kw['limit']]
+        return MemResultSet(results, include_docs)
 
     def design_view_create(self, design, views, syncwait=5):
         for v,d in views.iteritems():
-            mapf = execjs.compile(''' {} var mapf = {}'''.format(emitjs, d['map']))
+            mapf = execjs.compile(mapwrap.replace('%MAPF%', d['map'].strip()))
             view = dict(mapf=mapf)
-            if 'reduce' in d:
-                redf = execjs.compile('''var redf = {}'''.format(d['reduce']))
-                view['redf'] = redf
             self.designs["/".join((design, v))] = view
 
     def view_create(self, design, name, mapf, redf=None, syncwait=5):
@@ -92,13 +136,6 @@ class MemConnection(BaseConnection):
         for k in self.designs.keys():
             if k.startswith(prefix):
                 del self.designs[k]
-
-
-
-
-
-
-
 
 
 class CouchbaseConnection(BaseConnection):
--- tests/test_view.py
+++ tests/test_view.py
@@ -5,11 +5,12 @@ from ..cushion.model import Model, DocTypeMismatch, DocTypeNotFound
 from ..cushion.field import (
     Field, TextField, IntegerField, FloatField, RefField, DateTimeField
     )
-from ..cushion.persist import set_connection, CouchbaseConnection, Persist
+from ..cushion.persist import set_connection, MemConnection, Persist
 from ..cushion.view import View, sync_all
 
 
-set_connection(CouchbaseConnection('lvlrtest', 'localhost', 'gogogogo'))
+mc = MemConnection()
+set_connection(mc)
 
 
 class Boogie(Model):
@@ -36,10 +37,20 @@ class Boogie(Model):
         }
         ''' )
 
+    by_composite_key = View(
+        'boog', 'by_comp',
+        '''
+        function(doc) {
+            if (doc.type == "boogie") {
+                emit([doc.n, doc.i], null)
+            }
+        }
+        ''' )
+
 
 def clean_out_db_docs():
     for d in Boogie.all_docs(wrapper=None):
-        Persist().delete(d.key)
+        Persist().delete(d.docid)
 
 
 class Outter(Model):
@@ -62,9 +73,28 @@ class TestField(unittest.TestCase):
         assert r.n == b0.n
         assert r.id == b0.id
 
+    def test_by_comp(self):
+        b0 = Boogie(n='one').save()
+        b1 = Boogie(n='two').save()
+        res = Boogie.by_composite_key(startkey=[b0.n, 37], endkey=[b0.n, 37],
+                include_docs=True)
+        r = res[0]
+        assert isinstance(r, Boogie), "not a Boogie"
+        assert r.n == b0.n
+        assert r.id == b0.id
 
-
-
-
+    def test_descend(self):
+        b0 = Boogie(n='one').save()
+        b1 = Boogie(n='two').save()
+        Boogie(n='z0').save()
+        Boogie(n='z2').save()
+        Boogie(n='z8').save()
+        Boogie(n='z9').save()
+        res = Boogie.by_composite_key(
+                startkey=['z9', 37], endkey=['tw', 37],
+                descending=True, include_docs=True)
+        self.assertEqual(len(res), 5)
+        r = res[0] # should be z9
+        assert 'z9' == r.n
 
 
