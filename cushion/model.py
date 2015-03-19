d0f7b48ea853981979596e941570bd02c93162cb
Jeremy Kelley <jeremy@33ad.org>
2015-03-19 12:35:10 -0500

    removing leftover debug statement

--- cushion/model.py
+++ cushion/model.py
@@ -89,7 +89,6 @@ class Model(object):
         doc = Persist().get(docid)
         if not doc: return None
         # only load fields with non None values
-        print "BUILDING DOC", doc
         if '_id' not in doc:
             doc['_id'] = docid
         return cls(
