db435c766766de3d87802eeb2cae6d5524484a17
Jeremy Kelley <jeremy@33ad.org>
2015-05-07 14:05:21 -0500

    provide more feedback on OptionField failure

--- cushion/field.py
+++ cushion/field.py
@@ -128,7 +128,8 @@ class OptionField(Field):
 
     def __verify_choice(self, ch):
         if ch is not None and ch not in self.__choices:
-            raise ValueError('Invalid choice. choice: {}'.format(ch))
+            raise ValueError('Invalid choice. choice: {} of type: {}'.format(
+                ch, type(ch) ))
         return unicode(ch)
 
     def __init__(self, default=None, choices=None):
